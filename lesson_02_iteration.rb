7e3ce6fa3a0a06f79e820b49dbff9d5e3f46b121
karashchuk <karashchuk@inbox.ru>
2016-08-21 16:23:09 +0300

    Сделал 3 из 6

--- lesson_02_iteration.rb
+++ lesson_02_iteration.rb
@@ -2,7 +2,7 @@
 #
 # У вас есть массив имён, например:
 #
-names = %w[ambientsketchbook Erik\ Wollo Brian\ Eno Evangelos\ Papathanassiou Shulman]
+names = %w[ambientsketchbook Erik\ Wollo Brian\ Eno Evangelos\ Papathanassiou Shulman geek]
 #
 # Пробегитесь по ним и выведите только те, длина которых меньше 10 символов и первая буква заглавная. При реализации условий постарайтесь каждое из них обернуть в отдельный метод.
-p names
+names.each {|name| p name  if name.length < 10 && name[0].ord < 91 && name[0].ord > 64}
