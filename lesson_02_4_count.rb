4a349c766537ad6c78803c5a8ce33ba1802f840b
karashchuk <karashchuk@inbox.ru>
2016-08-21 18:52:24 +0300

    Все задания

--- lesson_02_4_count.rb
+++ lesson_02_4_count.rb
@@ -13,19 +13,13 @@ aa = ['Ruby','Python',"JavaScript", 'Java', ".NET", "HTML", "Clojure"]
 #
 # {"Ruby"=>4, "Python"=>6, "JavaScript"=>10, "Java"=>4, ".NET"=>4, "HTML"=>4, "Clojure"=>7, "Go"=>2}
 #
+hh = {}
+aa.each {|lang| hh.merge!({lang=>lang.length})}
+p hh
 
 hhh = aa.inject({}) do |memo,lang|
   memo.merge({lang=>lang.length})
 end
 p hhh
-
-hh = {}
-# aa.each do |lang|
-#   k = {lang=>lang.length}
-#   hh.merge!(k)
-# end
-aa.each {|lang| hh.merge!({lang=>lang.length})}
-#p aa
-p hh
 
 p (aa.inject({}) {|memo,lang| memo.merge({lang=>lang.length})})
--- lesson_02_Cesar.rb
+++ lesson_02_Cesar.rb
@@ -2,5 +2,30 @@
 #
 # Напишите алгоритм rot13, позволяющий кодировать и декодировать текст на английском языке. Например:
 #
-# cypher = 'Lbh unpxrq n irel frperg zrffntr!'
+# cypher = 'lbh unpxrq n irel frperg zrffntr!'
 # rot13(cypher) # => ?
+# aa = ('a'..'z').to_a
+# p aa
+def cezar (string, n)
+  aa = ('a'..'z').to_a
+  result = ""
+  i = 0
+  #print string.length
+  while i<string.length
+    if aa.include?(string[i])
+      new_index=aa.index(string[i]) + n
+      if new_index > 25
+        new_index = new_index - 26
+      elsif new_index <0
+        new_index = new_index+26
+      end
+      result += aa[new_index]
+    else
+      result += (string[i])
+    end
+  i+=1
+  end
+  result
+end
+
+print cezar('lbh unpxrq n irel frperg zrffntr!',13)
